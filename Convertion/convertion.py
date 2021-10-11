522794699f477045c4ff9ea0129b21dc9121c6c4
Antonio Valletta <61507922+Antoniovalletta7@users.noreply.github.com>
2021-10-11 14:49:51 +0200

    Add files via upload

--- Convertion/convertion.py
+++ Convertion/convertion.py
@@ -1,1 +1,7 @@
+import fastparquet
 
+pfile = fastparquet.ParquetFile('../recs/cf/runs.parquet')
+with open ('runs.csv', 'w') as fp:
+    for i, df in enumerate(pfile.iter_row_groups()):
+        write_header = (i==0)
+        df.to_csv(fp, index=False, header=write_header)
